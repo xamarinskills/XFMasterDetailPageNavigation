4010a41653d1c76a95eb8d565820788551992cc1
xamarinskills <sumitsisodia.xamarinskills@gmail.com>
2018-03-25 02:06:11 -0700

    First Commit

---
+++ XFMasterDetailPageNavigation/XFMasterDetailPageNavigation/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFMasterDetailPageNavigation.MenuItems;
+
+namespace XFMasterDetailPageNavigation
+{
+	public partial class MainPage : MasterDetailPage
+	{
+        public List<MasterPageItem> menuList { get; set; }
+        public MainPage()
+		{
+			InitializeComponent();
+
+            menuList = new List<MasterPageItem>();
+
+            // Adding menu items to menuList and you can define title ,page and icon
+            menuList.Add(new MasterPageItem() { Title = "Home", Icon = "home.png", TargetType = typeof(HomePage) });
+            menuList.Add(new MasterPageItem() { Title = "Setting", Icon = "setting.png", TargetType = typeof(SettingPage) });
+            menuList.Add(new MasterPageItem() { Title = "Help", Icon = "help.png", TargetType = typeof(HelpPage) });
+            menuList.Add(new MasterPageItem() { Title = "LogOut", Icon = "logout.png", TargetType = typeof(LogoutPage) });
+
+            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            navigationDrawerList.ItemsSource = menuList;
+
+            // Initial navigation, this can be used for our home page
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(HomePage)));
+        }
+
+        // Event for Menu Item selection, here we are going to handle navigation based
+        // on user selection in menu ListView
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
---
+++ XFMasterDetailPageNavigation/XFMasterDetailPageNavigation/MenuItems/MasterPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFMasterDetailPageNavigation.MenuItems
+{
+   public class MasterPageItem
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
